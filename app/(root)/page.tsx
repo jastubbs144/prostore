629fc94e1b41430bcdd4e5ccdf0e129c18b442ac
Joseph Stubbs <joseph.stubbs@prestige-nursing.co.uk>
2025-12-05 14:06:22 +0000

    added constants and env

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: 'Home'
+}
+
+const Home = () => {
+  return ( <>
+  <>Prostore</>
+  </> );
+}
+ 
+export default Home;
--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "@/components/ui/button";
-
-const Home = () => {
-  return ( <>
-  <Button>ShadCN first button</Button>
-  </> );
-}
- 
-export default Home;
